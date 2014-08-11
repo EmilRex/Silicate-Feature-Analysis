5a03ebf67afeda47eeb5aea6bb4ad9adda8ab8b3
EmilRex <chris2er@dukes.jmu.edu>
2014-08-11 14:16:48 -0400

    Cleaned up directory; removed irrelevant files

--- test.py
+++
@@ -1,9 +0,0 @@
-import astropy.io.fits as fits
-import matplotlib.pyplot as plt 
-
-hdu = fits.open('output_v2/HD117214_chn_mcmc_multi_mips_part.fits')
-
-data = hdu[0].data
-
-
-#plt.plot(data(1,),data(2,))
